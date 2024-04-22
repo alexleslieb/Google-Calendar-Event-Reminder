f0eae5b9f1010c287d6a92e8b5fea50d129c6f86
Alex Leslie <AlexL@bcicjamaica.com>
2024-04-22 02:00:48 -0500

    Added appsettings; added configurations/packages; added file for db context

---
+++ Event_Reminder/Entities/EventReminderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Event_Reminder.Entities
+{
+    public class EventReminderDbContext: DbContext
+    {
+        public EventReminderDbContext(DbContextOptions<EventReminderDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+
+        }
+    }
+}
--- Event_Reminder/Program.cs
+++ Event_Reminder/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Event_Reminder.Entities;
+
+var configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json");
+configurationBuilder.AddUserSecrets(Assembly.GetExecutingAssembly());
+var configuration = configurationBuilder.Build();
+
+HostApplicationBuilder hostApplicationBuilder = Host.CreateApplicationBuilder();
+hostApplicationBuilder.Logging.AddConsole();
+hostApplicationBuilder.Services.AddSingleton(configuration);
+
+hostApplicationBuilder.Services.AddDbContext<EventReminderDbContext>(options =>
+options.UseSqlServer(configuration.GetConnectionString("Event_Reminder")
+));
+
+hostApplicationBuilder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+var host = hostApplicationBuilder.Build();
+
+var mediator = host.Services.GetRequiredService<IMediator>();
